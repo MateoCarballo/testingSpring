d609353bb3acb923891e342c35073c79bc996118
Adrián Rodríguez Roqueiro <a18adrianrr@teis.intranet>
2026-03-11 20:09:02 +0100

    ni ctrl c y v

--- src/main/java/com/example/Ej501/SaludoController.java
+++ src/main/java/com/example/Ej501/SaludoController.java
@@ -1,11 +1,9 @@
 package com.example.Ej501;
 
+import com.example.Ej501.SaludoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/saludo")
--- src/main/java/com/example/Ej501/SaludoService.java
+++ src/main/java/com/example/Ej501/SaludoService.java
@@ -2,8 +2,6 @@ package com.example.Ej501;
 
 import org.springframework.stereotype.Service;
 
-import org.springframework.stereotype.Service;
-
 @Service
 public class SaludoService {
     public String saludarUsuario(String nombre) {
